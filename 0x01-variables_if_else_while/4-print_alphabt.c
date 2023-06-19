79304162a27e073c9ba99541cfd2ecc0a3e6bce3
Jiboga007 <jibogz@gmail.com>
2023-06-19 15:31:12 -0700

    My 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,15 +6,13 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char low, e, q;
 
 	e = 'e';
 	q = 'q';
-
-	for (low = 'a'; low <= 'Z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
 	{
 	if (low != e && low != q)
 	putchar(low);
