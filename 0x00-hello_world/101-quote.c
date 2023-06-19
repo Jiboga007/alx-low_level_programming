9cd2fe1fffab67f6af646a652f470aa398a2726b
Jiboga007 <jibogz@gmail.com>
2023-06-19 06:16:40 -0700

    My 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
+ * main - A c program that prints a line to the standard
  * Return: 1 Always (Success)
  */
 int main(void)
